c37a7732b9d48135e71db5dee15693602b28545e
Dennis H <dennis.heimbert@gmail.com>
2015-03-16 20:39:56 +0100

    fix q3s

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -49,10 +49,11 @@ zcalcb () { print $(( [#2] ans = ${1:-ans} )) }
 
 q3s() {
   echo "\xff\xff\xff\xffgetstatus" | \
-  socat - udp4:$1 | \
+  socat - udp4:$1 2>/dev/null | \
   sed -e '1d' \
       -e '2s/\\\([^\]*\)\\\([^\]*\)/\1=\2\n/g' \
       -e 's/\^[0-9a-zA-Z]//g'
+  return $pipestatus[2]
 }
 
 # vim:ft=zsh
